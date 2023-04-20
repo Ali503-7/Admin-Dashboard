7122c4c0d66260164ac5fe22d6eb45284761fd40
Ali503-7 <alimoali354@gmail.com>
2023-04-20 13:05:09 +0200

    add scss and sass with tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=10")
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
-    <div className="App">
-      <h1 className='text-red-500'>nice to meet you Tailwind</h1>
+    <div className="tailwind">
+      <h1 className="text-red-500">
+        nice to meet you Tailwind
+      </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
