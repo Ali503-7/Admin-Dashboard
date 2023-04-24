a42056b423170330f968a43c38c6c260a4b6a3aa
Ali503-7 <alimoali354@gmail.com>
2023-04-24 13:08:01 +0200

    make the input_text components to make it easyer to make it any where in the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,8 @@
-import { useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronRight, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import Input_text from "./components/Bata_components/input_text";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="tailwind">
-      <div className="input-text">
-        <label htmlFor="Text" className="block">
-          Text Label
-        </label>
-        <div className="input">
-          <div className="env right-2">
-            <FontAwesomeIcon icon={faEnvelope} />
-          </div>
-          <input type="text" name="Text" id="Text" placeholder="Text field" className="!pr-10"/>
-        </div>
-      </div>
-    </div>
-  );
+  return <Input_text />;
 }
 
 export default App;
---
+++ src/components/Bata_components/input_text.jsx
@@ -0,0 +1,42 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+function Input_text({ env, env_right, label, check_box, arrows }) {
+  return (
+    <div className="input-text">
+      {label && (
+        <label htmlFor="Text" className="block">
+          {label}
+        </label>
+      )}
+      <div className="input">
+        {env && (
+          <div className={env_right ? "env right-2" : "env left-2"}>
+            <FontAwesomeIcon icon={faEnvelope} />
+          </div>
+        )}
+        {check_box && (
+          <label className="Check">
+            <input type="checkbox" />
+            <span className="checkmark"></span>
+          </label>
+        )}
+        {arrows && (
+          <div className="arrows">
+            <FontAwesomeIcon icon={faChevronRight} />
+            <FontAwesomeIcon icon={faChevronRight} />
+          </div>
+        )}
+        <input
+          type="text"
+          name="Text"
+          id="Text"
+          placeholder="Text field"
+          className={env || env_right || check_box ? "!pl-8" : ""}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Input_text;
