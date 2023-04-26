6ce0f3b366da4ae6f5e3ae4747c2048ab35a95e2
Ali503-7 <alimoali354@gmail.com>
2023-04-26 07:39:06 +0200

    Section_1 styling is all done

--- src/components/Bata_components/Product_box/Product_box.jsx
+++ src/components/Bata_components/Product_box/Product_box.jsx
@@ -19,19 +19,30 @@ const Product_box = () => {
 
   const Products = () => {
     return data ? (
-      data.map((item) => (
-        <div className="product_box" key={item.id}>
-          <div className="">
-            <img src={item.image} alt="" />
+      data.slice(0, 6).map((item) => (
+        <div
+          className="product_box h-fit w-[269px] rounded-xl border border-c1-D p-4 mb-20"
+          key={item.id}
+        >
+          <div className="mb-4">
+            <img
+              src={item.image}
+              alt={item.title}
+              className="h-[180px] w-[237px]"
+            />
           </div>
-          <div className="">
-            <div className="">
-              <h4>{item.title}</h4>
-              <p>{item.description}</p>
+          <div>
+            <div className="mb-4">
+              <h4 className="truncate font-poppins text-[15px] font-[500] text-c1-A">
+                {item.title}
+              </h4>
+              <p className="h-5 overflow-hidden truncate font-sans text-[12px]">
+                {item.description}
+              </p>
             </div>
-            <div className="">
-              <div className="">
-                <p>{item.price}</p>
+            <div className="flex flex-row justify-between items-center">
+              <div className="text-[18px] text-c1-A font-poppins font-[600]">
+                <span className="mr-1">{item.price}</span>
                 <span>USD</span>
               </div>
               <button className="btn-c2 btn-sm">Buy now</button>
@@ -44,7 +55,11 @@ const Product_box = () => {
     );
   };
 
-  return <div className="">{Products()}</div>;
+  return (
+    <div className="mt-[125px] flex flex-wrap justify-between">
+      {Products()}
+    </div>
+  );
 };
 
 export default Product_box;
