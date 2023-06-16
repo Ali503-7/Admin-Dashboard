4ae68beefaf6af6f485a0af4357933d3f6736419
Ali503-7 <alimoali354@gmail.com>
2023-06-16 14:59:26 +0300

    add all the fonts and start in the box shadows

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { createBrowserRouter } from "react-router-dom"
-function App() {                            
-  return <h1 className='bg-Light-Custom-Error'>Tailwind</h1>
+import { createBrowserRouter } from "react-router-dom";
+function App() {
+  return <h1 className="bg-Light-Custom-Error H1 ">Tailwind</h1>;
 }
 
-export default App
+export default App;
